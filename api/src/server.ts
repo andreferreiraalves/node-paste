7449a39c208702829b0c45feb3ebdfd91f9dddc3
André Ferreira <andre.ferreira.alves@gmail.com>
2024-03-25 20:19:42 -0300

    dotenv

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,12 +1,15 @@
+import 'dotenv/config';
 import fastify from "fastify";
 import routers from './routes';
+
+const PORT = process.env.API_PORT
 
 const app = fastify()
 
 app.register(routers)
 
 app.listen({
-    port: 3000
+    port: parseInt(PORT ?? '3000'),
 }).then(() => {
-    console.log('server running on port 3000')
+    console.log(`server running on port ${PORT}`)
 })
