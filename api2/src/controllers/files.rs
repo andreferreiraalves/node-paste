d68e6aa232e9fc19aad873f0c7a29301dedad501
André Ferreira <andre.ferreira.alves@gmail.com>
2024-08-08 16:29:34 -0300

    download files

--- api2/src/controllers/files.rs
+++ api2/src/controllers/files.rs
@@ -1,18 +1,18 @@
 use std::path::PathBuf;
 
 use actix_multipart::form::MultipartForm;
-use actix_web::{get, post, web, Error, HttpResponse, Result};
+use actix_web::{get, post, web, Error, HttpResponse, Responder, Result};
 use serde_json::json;
 use tokio::{fs::File, io::AsyncReadExt};
 
-use crate::{schema::UploadForm, AppState};
+use crate::{model::RecordModel, schema::UploadForm, AppState};
 
 #[post("/upload")]
 async fn upload(
     MultipartForm(form): MultipartForm<UploadForm>,
     data: web::Data<AppState>,
     // ) -> Result<HttpResponse, Error> {
-) -> HttpResponse {
+) -> impl Responder {
     let new_guid = uuid::Uuid::new_v4();
 
     for f in form.files {
@@ -46,23 +46,69 @@ async fn upload(
     }))
 }
 
-#[get("/download/{filename}")]
-async fn download(filename: web::Path<String>) -> Result<HttpResponse> {
-    let file_path: PathBuf = format!("./temp/{}", filename).into();
+#[get("/download/{guid}")]
+async fn download(guid: web::Path<uuid::Uuid>, data: web::Data<AppState>) -> impl Responder {
+    let id = guid.into_inner();
 
-    let mut file = File::open(&file_path)
+    let file_record_result = sqlx::query_as!(
+        RecordModel,
+        r"select * from records where id = $1 and FILE_NAME is not null",
+        id
+    )
+    .fetch_one(&data.db)
+    .await
+    .map_err(|e| e.to_string());
+
+    let result = match file_record_result {
+        Ok(record) => record,
+        Err(err) => {
+            return HttpResponse::BadRequest().json(serde_json::json!({"message": err}));
+        }
+    };
+
+    let file_path: PathBuf = format!("./temp/{}", result.id.to_string()).into();
+
+    let file_result = File::open(&file_path)
         .await
-        .map_err(actix_web::error::ErrorInternalServerError)?;
+        .map_err(actix_web::error::ErrorInternalServerError);
+
+    let mut file = match file_result {
+        Ok(file) => file,
+        Err(_) => {
+            return HttpResponse::InternalServerError()
+                .json(serde_json::json!({"message":"file not found"}));
+        }
+    };
 
     let mut buffer = Vec::new();
+
     file.read_to_end(&mut buffer)
         .await
-        .map_err(actix_web::error::ErrorInternalServerError)?;
+        .map_err(actix_web::error::ErrorInternalServerError)
+        .unwrap();
 
-    Ok(HttpResponse::Ok()
+    let file_name = format!("attachment; filename={}", result.file_name.unwrap());
+
+    HttpResponse::Ok()
         .content_type("application/octet-stream")
-        .insert_header(("Content-Disposition", "attachment; filename=file.txt"))
-        .body(buffer))
+        .insert_header(("Content-Disposition", file_name))
+        .body(buffer)
+
+    // let file_path: PathBuf = format!("./temp/{}", filename).into();
+
+    // let mut file = File::open(&file_path)
+    //     .await
+    //     .map_err(actix_web::error::ErrorInternalServerError)?;
+
+    // let mut buffer = Vec::new();
+    // file.read_to_end(&mut buffer)
+    //     .await
+    //     .map_err(actix_web::error::ErrorInternalServerError)?;
+
+    // Ok(HttpResponse::Ok()
+    //     .content_type("application/octet-stream")
+    //     .insert_header(("Content-Disposition", "attachment; filename=file.txt"))
+    //     .body(buffer))
 }
 
 pub fn config(conf: &mut web::ServiceConfig) {
